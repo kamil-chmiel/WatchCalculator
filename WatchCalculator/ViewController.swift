b79924b9b0de508bb168281a238a9fd9fa1638b1
Kamil Chmiel <kamil.chmiel@ready4s.com>
2019-08-25 14:29:06 +0200

    Initial commit

---
+++ WatchCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchCalculator
+//
+//  Created by Kamil Chmiel on 24/08/2019.
+//  Copyright © 2019 kamilchmiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
